e59df9e9ab1b3f34fbb49eb370f76dc05719fb15
Ayyub Ibrahim <ayyub@Ayyubs-MacBook-Pro.local>
2023-11-26 13:50:47 -0600

    expanded on model

--- src/src.py
+++ src/src.py
@@ -1,110 +1,86 @@
 import cv2
 import base64
-import requests
+import os
 import csv
+from langchain.chat_models import ChatOpenAI
+from langchain.schema.messages import HumanMessage
 
 
-def encode_image(image_path):
-    """Encode the image in base64."""
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode("utf-8")
+def encode_frame(frame):
+    """Encode a video frame to base64"""
+    _, buffer = cv2.imencode(".jpg", frame)
+    return base64.b64encode(buffer).decode("utf-8")
 
+def frame_summarize(frame_base64, prompt):
+    """Summarize a video frame"""
+    chat = ChatOpenAI(model="gpt-4-vision-preview", max_tokens=1024, api_key="")
+    msg = chat.invoke(
+        [
+            HumanMessage(
+                content=[
+                    {"type": "text", "text": prompt},
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{frame_base64}"},
+                    },
+                ]
+            )
+        ]
+    )
+    return msg.content
 
-def extract_and_encode_specific_frames(video_path, start_time, end_time, fps):
-    """Extract and encode specific frames from a video."""
-    encoded_frames = []
-    timestamps = []
-    start_frame = int(fps * start_time)
-    end_frame = int(fps * end_time)
+def generate_frame_summaries(video_path, start_second, end_second, fps=30):
+    """
+    Generate summaries for specific frames in a video.
+    video_path: Path to the video file.
+    start_second: Start time in seconds for analysis.
+    end_second: End time in seconds for analysis.
+    fps: Frame rate of the video.
+    """
     video = cv2.VideoCapture(video_path)
-    video.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
 
-    while video.isOpened() and video.get(cv2.CAP_PROP_POS_FRAMES) <= end_frame:
+    # Calculate start and end frame numbers
+    start_frame = start_second * fps
+    end_frame = end_second * fps
+
+    prompt = """What’s in this image? Please describe any actions and postures. 
+                Format your response in accordance with the guidelines below.
+                
+                Summary: this should be an overall summary of the image
+                Individual 1: this should be a description of the main individual present
+                
+                If there is more than one individual include them as well, for example:
+                Individual 2:, Individual 3:, Individual 4:, etc"""
+    frame_count = 0
+    output_data = []
+    while video.isOpened():
         success, frame = video.read()
         if not success:
             break
-        if int(video.get(cv2.CAP_PROP_POS_FRAMES)) % fps == 0:
-            timestamp = video.get(cv2.CAP_PROP_POS_FRAMES) / fps
-            frame_path = f"../data/frames/frame_{int(timestamp)}.jpg"
-            cv2.imwrite(frame_path, frame)
-            encoded_frame = encode_image(frame_path)
-            encoded_frames.append(encoded_frame)
-            timestamps.append(timestamp)
+        if frame_count >= start_frame and frame_count <= end_frame:
+            if frame_count % fps == 0:  # Capture one frame per second
+                base64_frame = encode_frame(frame)
+                summary = frame_summarize(base64_frame, prompt)
+                timestamp = frame_count / fps
+                output_data.append([timestamp, summary])
+        frame_count += 1
+        if frame_count > end_frame:
+            break
 
     video.release()
-    return encoded_frames, timestamps
+    return output_data
 
-
-def analyze_frames_and_write_to_csv(encoded_frames, timestamps, headers, csv_file):
-    """Analyze each frame and write the results to a CSV file."""
-    with open(csv_file, mode="w", newline="", encoding="utf-8") as file:
+def save_to_csv(data, filename):
+    """Save data to a CSV file"""
+    with open(filename, 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        writer.writerow(["timestamp", "analysis"])
+        writer.writerow(["Timestamp", "Summary"])
+        writer.writerows(data)
 
-        for encoded_frame, timestamp in zip(encoded_frames, timestamps):
-            payload = {
-                "model": "gpt-4-vision-preview",
-                "messages": [
-                    {
-                        "role": "user",
-                        "content": [
-                            {
-                                "type": "text",
-                                "text": """What’s in this image? Please describe any actions and postures. 
-                               Format your response in accordance with the guidelines below.
-                               
-                               Summary: this should be an overall summary of the image
+# Example usage
+video_path = "../data/input/boser.mp4"
+output_data = generate_frame_summaries(video_path, 38, 45)
+csv_filename = "../data/output/video_summaries.csv"
+save_to_csv(output_data, csv_filename)
 
-                               Individual 1: this should be a description of the main individual present
-                               
-                               If there is more than one individual include them as well, for example:
-                               
-                               Individual 2: """,
-                            },
-                            {
-                                "type": "image_url",
-                                "image_url": {
-                                    "url": f"data:image/jpeg;base64,{encoded_frame}"
-                                },
-                            },
-                        ],
-                    }
-                ],
-                "max_tokens": 300,
-            }
-
-            response = requests.post(
-                "https://api.openai.com/v1/chat/completions",
-                headers=headers,
-                json=payload,
-            )
-            response_content = (
-                response.json()
-                .get("choices", [{}])[0]
-                .get("message", {})
-                .get("content", "")
-            )
-            print(response_content)
-            writer.writerow([timestamp, response_content])
-
-
-def main():
-    """Main function to execute the script."""
-    video_path = "../data/boser.mp4"
-    start_time, end_time = 38, 45
-    csv_file = "../data/output/analysis_output.csv"
-
-    api_key = ""
-    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
-
-    video = cv2.VideoCapture(video_path)
-    fps = video.get(cv2.CAP_PROP_FPS)
-    video.release()
-
-    encoded_frames, timestamps = extract_and_encode_specific_frames(
-        video_path, start_time, end_time, fps
-    )
-    analyze_frames_and_write_to_csv(encoded_frames, timestamps, headers, csv_file)
-
-if __name__ == "__main__":
-    main()
+print(f"Summaries saved to {csv_filename}")
